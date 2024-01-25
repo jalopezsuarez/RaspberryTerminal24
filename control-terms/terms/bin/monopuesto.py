688d0883a8d5d8214a7c795a608898fef430e31f
Jose Antonio Lopez <jalopezsuarez@users.noreply.github.com>
2024-01-25 20:21:08 +0100

    Update monopuesto.py

--- control-terms/terms/bin/monopuesto.py
+++ control-terms/terms/bin/monopuesto.py
@@ -1,13 +1,11 @@
 import sys
 import os
-import shutil
-import mmap
 
-remmina = ('/home/pi/.local/share/remmina/')
+remmina = '/home/pi/.local/share/remmina/'
 
 resources = os.listdir(remmina)
 for filename in resources:
     if filename.endswith('.remmina'):
-        with open(remmina + filename,'r') as f:
-            print remmina + filename
-            sys.exit(0)
+        filepath = os.path.join(remmina, filename)
+        print(filepath)
+        sys.exit(0)
